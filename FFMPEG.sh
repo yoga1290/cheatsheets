990dd6553d70972055b68ab9efd219e1d27e8a63
Youssef Gamil <yoga1290@gmail.com>
2017-12-24 14:23:11 +0200

    Update FFMPEG.sh

--- FFMPEG.sh
+++ FFMPEG.sh
@@ -8,6 +8,28 @@ ffmpeg -i $INPUT -filter_complex loop=$LOOPS:$FR_SIZE:$FR_START $OUTPUT
 ffmpeg -t $DURATION -r $FR_RATE -f avfoundation -i "default" $OUTPUT.mpg
 # https://trac.ffmpeg.org/wiki/Capture/Webcam#OSX
 
-ffmpeg -i $INPUT.mov -i $INPUT2.mp4 -filter_complex "[0]colorkey=color=$COLOR_KEY:similarity=$SIML[keyed];[1][keyed]overlay" $OUTPUT
-# https://ffmpeg.org/ffmpeg-filters.html#colorkey
+# Scaling & Padding
+# https://stackoverflow.com/a/8351875/1683797
+scale="scale=\
+  iw*min($width/iw\,$height/ih):ih*min($width/iw\,$height/ih),\
+  pad=\
+  $width:$height:($width-iw)/2:($height-ih)/2"
+ffmpeg \
+-y \
+-i $INPUT \
+-vf $scale \
+$OUTPUT;
 
+# Chroma-key (green screen effect):
+# https://ffmpeg.org/ffmpeg-filters.html#colorkey
+#COLOR_KEY='green'
+#SIML=0.1
+ffmpeg \
+-i $INPUT \
+-i $INPUT2 \
+-filter_complex \
+"[1] $scale[in1] ;\
+[0] colorkey=color=$COLOR_KEY:similarity=$SIML [keyed];\
+[in1][keyed]overlay" \
+-y \
+$OUTPUT
