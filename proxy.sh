53a3af43e0ea7a191d8ff0e93cfb790e4321a15d
Youssef Gamil <yoga1290@gmail.com>
2018-03-05 14:55:32 +0200

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -2,8 +2,8 @@
 
 export http_proxy='http://DOMAIN:PORT';
 export https_proxy='http://DOMAIN:PORT';
-export HTTP_PROXY='http://DOMAIN:PORT';
-export HTTPS_PROXY='http://DOMAIN:PORT';
+export HTTP_PROXY=$http_proxy;
+export HTTPS_PROXY=$https_proxy;
 
 # Ubuntu/ apt-get
 echo "Acquire::http::Proxy \"$http_proxy\";" > /etc/apt/apt.conf
@@ -12,12 +12,12 @@ echo "Acquire::http::Proxy \"$http_proxy\";" > /etc/apt/apt.conf
 mkdir -p /etc/systemd/system/docker.service.d;
 CONFIG_FILE='/etc/systemd/system/docker.service.d/http-proxy.conf';
 echo "[Service]\n" >$CONFIG_FILE;
-echo 'Environment="HTTP_PROXY=http://DOMAIN:PORT" "NO_PROXY=localhost,127.0.0.1,docker-registry.somecorporation.com"' >>$CONFIG_FILE;
+echo "Environment=\"HTTP_PROXY=$http_proxy\" \"NO_PROXY=localhost,127.0.0.1,docker-registry.somecorporation.com\"" >>$CONFIG_FILE;
 
-echo "HTTP_PROXY=$HTTP_PROXY" >/root/.docker/proxy.env
-echo "HTTPS_PROXY=$HTTPS_PROXY" >>/root/.docker/proxy.env
-echo "http_proxy=$http_proxy" >>/root/.docker/proxy.env
-echo "https_proxy=$https_proxy" >>/root/.docker/proxy.env
+echo "HTTP_PROXY=$HTTP_PROXY" >/home/$USER/.docker/proxy.env
+echo "HTTPS_PROXY=$HTTPS_PROXY" >>/home/$USER/.docker/proxy.env
+echo "http_proxy=$http_proxy" >>/home/$USER/.docker/proxy.env
+echo "https_proxy=$https_proxy" >>/home/$USER/.docker/proxy.env
 # to be passed while starting containers:
 # e.g: docker run --env-file ~/.docker/proxy.env -it -p 8000:8000 --net="host" --rm --name container web-portal
 systemctl daemon-reload;
@@ -33,6 +33,6 @@ npm config set https-proxy $HTTPS_PROXY;
 
 # BOWER:
 echo '{' >~/.bowerrc;
-echo '  "proxy": "http://DOMAIN:PORT/",' >>~/.bowerrc;
-echo '  "https-proxy":"http://DOMAIN:PORT/"' >>~/.bowerrc;
+echo "  \"proxy\": \"$http_proxy\"," >>~/.bowerrc;
+echo "  \"https-proxy\": \"$https_proxy\"," >>~/.bowerrc;
 echo '}' >>~/.bowerrc;
